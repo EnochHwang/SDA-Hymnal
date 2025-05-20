ac2e562fefac4ae9bf116bc9bdce6e4b12b11100
Enoch <enochhwangphd@gmail.com>
2025-05-19 20:19:40 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,9 @@
-const CACHE_NAME = 'piano-book-v2';
-const CORE_FILES = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_FILES))
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        if (event.request.url.endsWith('.png')) {
-          const responseClone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        }
-        return response;
-      }).catch(() => cached);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
